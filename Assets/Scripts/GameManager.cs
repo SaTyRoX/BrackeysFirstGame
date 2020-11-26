68eb1157e2d76c34a82bdf3dcff0299db38ee120
Benjamin Jensen <Benjaminmargon@hotmail.com>
2020-11-26 11:04:36 +0100

    Tilføjer BrackeysFirstGame

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+
+    bool gameHasEnded = false;
+
+    public float restartDelay = 1;
+
+    public GameObject completeLevelUI;
+
+    public void CompleteLevel () {
+        completeLevelUI.SetActive(true);
+    }
+
+    public void EndGame () {
+
+        if (gameHasEnded == false) {
+
+            gameHasEnded = true;
+            Debug.Log("Game over");
+            Invoke("restart", restartDelay);
+        }
+    }
+
+    void restart() {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+}
